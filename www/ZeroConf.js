62cb5a77e6e76a8761e0def2fa5eb6ea03e5a7ab
Vlad Stirbu <vstirbu@gmail.com>
2014-04-14 19:26:12 +0300

    refactored to comply with jshint

--- www/ZeroConf.js
+++ www/ZeroConf.js
@@ -9,36 +9,40 @@
  * Available under the terms of the MIT License.
  * 
  */
+
+/*global module, console, require*/
+/*jshint -W097 */
+'use strict';
 var exec = require('cordova/exec');
 
 var ZeroConf = {
-	watch: function(type, callback) {
-		return exec(function(result) {
-			if(callback) {
+	watch: function (type, callback) {
+		return exec(function (result) {
+			if (callback) {
 				callback(result);
 			}
 
 		}, ZeroConf.fail, "ZeroConf", "watch", [type]);
 	},
-	unwatch: function(type) {
+	unwatch: function (type) {
 		return exec(null, ZeroConf.fail, "ZeroConf", "unwatch", [type]);
 	},
-	close: function() {
-		return exec(null, ZeroConf.fail, "ZeroConf", "close", [])
+	close: function () {
+		return exec(null, ZeroConf.fail, "ZeroConf", "close", []);
 	},
-	register: function(type, name, port, text) {
-		if(!type) {
+	register: function (type, name, port, text) {
+		if (!type) {
 			console.error("'type' is a required field");
 			return;
 		}
 		return exec(null, ZeroConf.fail, "ZeroConf", "register", [type, name, port, text]);
 	},
-	unregister: function() {
-		return exec(null, ZeroConf.fail, "ZeroConf", "unregister", [])
+	unregister: function () {
+		return exec(null, ZeroConf.fail, "ZeroConf", "unregister", []);
 	},
 	fail: function (o) {
 		console.error("Error " + JSON.stringify(o));
 	}
-}
+};
 
 module.exports = ZeroConf;
